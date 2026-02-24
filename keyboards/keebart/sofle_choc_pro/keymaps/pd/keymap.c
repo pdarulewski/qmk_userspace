f30152dfc2ac5f6bc26afc358c5a839e993a07a3
Pawel Darulewski <pawel.darulewski@gmail.com>
2026-02-24 19:20:35 +0100

    feat: reboot key

--- keyboards/keebart/sofle_choc_pro/keymaps/pd/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/pd/keymap.c
@@ -26,10 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_ESC,  MTQ_A,   MTQ_S,   MTQ_D,   MTQ_F,   KC_G,                           KC_H,    MTQ_J,   MTQ_K,   MTQ_L,   MTQ_SCLN,KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    C(KC_A),      KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    C(KC_A),      KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LGUI, KC_LALT, KC_LCTL, MO(SYM), KC_ENT,       KC_SPC,  MO(FUN), KC_RCTL, KC_RALT, KC_RGUI
     ),
-
 /*
           ~ c o l e m a k  d h ~                       ┆
 ┌─────────┬─────────┬─────────┬─────────┬─────────┬────╨────┐                      ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐
@@ -50,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                           KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
         KC_ESC,  MTC_A,   MTC_R,   MTC_S,   MTC_T,   KC_G,                           KC_M,    MTC_N,   MTC_E,   MTC_I,   MTC_O,   KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_BSPC,      KC_BSPC, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_BSPC,      KC_BSPC, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LGUI, KC_LALT, KC_LCTL, MO(SYM), KC_ENT,       KC_SPC,  MO(FUN), KC_RCTL, KC_RALT, KC_RGUI
     ),
 /*
@@ -96,15 +95,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RM_TOGG, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9 ,  KC_F10,  KC_F11,
         RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, KC_VOLU,                        KC_MPRV, KC_MNXT, _______, _______, _______, KC_F12,
         RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, KC_VOLD,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        DF(QWE), DF(COL), QK_BOOT, _______, _______, _______, KC_MUTE,      KC_MPLY, _______, _______, _______, _______, _______, _______,
-                          KC_LGUI, KC_LALT, KC_LCTL, _______, KC_ENT,       KC_SPC,  _______, KC_RCTL, KC_RALT, KC_RGUI
+        DF(QWE), DF(COL), _______, _______, _______, _______, KC_MUTE,      KC_MPLY, _______, _______, _______, _______, _______, _______,
+                          QK_BOOT,QK_REBOOT,_______, _______, KC_ENT,       KC_SPC,  _______, _______, _______,_______
     ),
 };
 
 void keyboard_post_init_user(void) {
     rgb_matrix_enable_noeeprom();
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
-    rgb_matrix_sethsv_noeeprom(HSV_CYAN);
+    rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
 }
 
 /* template
